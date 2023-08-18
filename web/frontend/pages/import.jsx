955bb50c9ef8fcdc74a52dfb3957ac5250c83a79
Samuel Di Quinzio <sammyd@samuels-air.home>
2023-08-18 18:36:47 -0400

    added button

--- web/frontend/pages/import.jsx
+++ web/frontend/pages/import.jsx
@@ -110,7 +110,7 @@ export default function Glitchingproducts() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold pt-8 pl-8 rounded">
               Import Products
             </button>
           </a>
